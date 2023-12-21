e47c3d9271403d671fa183a650c20e24e1e4b2f8
mavart2 <mavart08@gmail.com>
2023-12-20 23:53:15 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CreaTuLanding_Vargas/',
+  base: 'https://mavart2.github.io/CreaTuLanding_Vargas/',
 })
